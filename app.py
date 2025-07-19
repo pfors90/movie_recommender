0c3fe91d1c4213f14e42aab7d29fa85135697a1b
Patrick Forsyth <pfors90@gmail.com>
2025-07-18 22:47:26 -0400

    convert to Flask webapp, modify db connections, tweak recommendation logic

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, abort, request
+
+import database_helper as dbh
+import vector_helper as vh
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/visualizations/")
+def visualizations():
+    #--TODO build out visualizations page
+    return render_template("visualizations.html")
+
+
+@app.route("/your_movies/", methods=["GET"])
+def your_movies():
+    # routing for the user movie entry page
+    return render_template("your_movies.html")
+
+@app.route("/parse_user_movies/", methods=["POST"])
+def parse_user_movies():
+    #--TODO: modify database to store a "normalized" movie title (remove spaces, punctuation, etc)
+    #--TODO: then normalize user queries and search the database for a partial/full match
+
+    # determines whether we can proceed with the recommendations or if we need clarification from the user
+
+    user_movie_titles = request.form.getlist("movie_titles")
+    user_movie_titles = [title.strip() for title in user_movie_titles if title.strip()]
+
+    exact_matches = []
+    partial_matches = []
+    missing_matches = []
+
+    for title in user_movie_titles:
+        # each result row is a Row_factory, contains "id", "title", "release_date"
+        # runs exact and approximate SELECT queries to try and find matches with slightly decreasing precision
+        results = dbh.get_potential_title_matches(title)
+
+        if len(results) == 1: # only one match in the database, grab it and go
+            exact_matches.append(results[0])
+
+        elif len(results) > 1: # multiple matches found
+            # store the user-entered title along with a list of the results to pass to front end for confirmation
+            partial_matches.append([title, results])
+
+        else: # no matches found
+            missing_matches.append(title)
+
+    # pass potential matches to a confirmation page for the user
+    if partial_matches or missing_matches:
+        return render_template("confirm.html", exact_matches = exact_matches, partial_matches = partial_matches, missing_matches = missing_matches)
+
+    elif exact_matches: # fallback condition: everything matched exactly
+        user_movie_ids = [int(movie["id"]) for movie in exact_matches]
+        return render_template("recommendations.html", user_movies = dbh.get_movies_by_ids(user_movie_ids), recs = vh.get_recommendations_by_ids(user_movie_ids))
+
+    else: abort(500)
+
+
+@app.route("/parse_user_movies/", methods=["GET"])
+def parse_user_movies_get(): # invalid request
+    abort(405)
+
+
+@app.route("/process_confirmation/", methods=["POST"])
+def process_confirmation():
+    user_movie_ids_raw = request.form.getlist("confirmed_ids")
+    user_movie_ids = [int(id) for id in user_movie_ids_raw]
+
+    for key in request.form:
+        if key.startswith("partial_"):
+            user_movie_ids.append(int(request.form[key]))
+    return render_template("recommendations.html", user_movies = dbh.get_movies_by_ids(user_movie_ids), recs = vh.get_recommendations_by_ids(user_movie_ids))
+
+
+@app.route("/process_confirmation/", methods=["GET"])
+def process_confirmations_get(): # invalid request
+    abort(405)
+
+
+@app.teardown_appcontext
+def teardown_db(exception): # closes db connection between page loads
+    dbh.close_db()
--- vector_helper.py
+++ vector_helper.py
@@ -1,27 +1,41 @@
+import database_helper as dbh
 from Movie import Movie
 
 def get_composite_by_vectors(vectors: list[dict], normalize=True) -> dict[int, float]:
+    """
+    :param vectors: a list of sparse vectors represented as dictionaries
+    :param normalize: boolean flag, whether to normalize the vector before returning (default True)
+    :return: a sparse vector representing the average of the supplied vectors
+    """
     if not vectors:
         return dict()
 
     composite = dict()
     for vector in vectors:
         for k, v in vector.items():
-            composite[k] = composite.get(k, 0.0) + v
+            composite[k] = composite.get(k, 0.0) + v # default 0.0 if the index isn't yet in the dict
 
     num_vectors = len(vectors)
     for k in composite:
         composite[k] /= num_vectors
 
-    if normalize:
+    if normalize: # default case
         return normalize_vector(composite)
 
-    return composite
+    return composite # non-normalized
 
 
 #--TODO: below function assumes all movies have a vector (before adding 'if' statement)
 #--TODO: warn user that a movie is not being tracked? how should we handle this?
+
+#--TODO NOTE: NEW PARSING LOGIC WILL EXCLUDE MOVIES WITH NO KEYWORDS FROM THE DATABASE
 def get_composite_by_movies(movies: list[Movie], normalize=True) -> dict[int, float]:
+    """
+    takes in a list of Movie objects, strips out the vectors, and calls get_composite_by_vectors()
+    :param movies: a list of Movie objects
+    :param normalize: boolean flag, whether to normalize resulting composite vector (default True)
+    :return: a sparse vector representing the average of the supplied vectors
+    """
     vectors = [movie.vector for movie in movies if movie.vector is not None]
 
     return get_composite_by_vectors(vectors, normalize=normalize)
@@ -32,6 +46,11 @@ def compute_norm(vector: dict[int, float]):
 
 
 def normalize_vector(vector: dict[int, float]) -> dict[int, float]:
+    """
+    normalization "removes" magnitude from vectors so that angles can be properly compared
+    :param vector: non-normalized sparse vector represented as a dictionary
+    :return: normalized sparse vector represented as a dictionary
+    """
     if not vector:
         return dict()
 
@@ -45,6 +64,15 @@ def normalize_vector(vector: dict[int, float]) -> dict[int, float]:
 
 # function by default assumes vectors are normalized - set normalize flag if not
 def cosine_similarity(vector1: dict[int, float], vector2: dict[int, float], normalized: bool = True) -> float:
+    """
+    takes in two vectors and calculates the cosine similarity, returning the value as a float\n
+    if only one vector is normalized, set 'normalized' to False - the already normalized vector will have a calculated
+    norm of 1 and not affect the logic
+    :param vector1: a sparse vector represented as a dictionary
+    :param vector2: a sparse vector represented as a dictionary
+    :param normalized: boolean flag, whether the two vectors are already normalized (default True)
+    :return: a float value representing the similarity score
+    """
     # every dimension not shared by the vectors will resolve to 0.0
     shared_dimensions = set(vector1).intersection(set(vector2))
     similarity_score = sum((vector1[d]*vector2[d] for d in shared_dimensions))
@@ -53,3 +81,38 @@ def cosine_similarity(vector1: dict[int, float], vector2: dict[int, float], norm
         return similarity_score
 
     return similarity_score / (compute_norm(vector1)*compute_norm(vector2))
+
+
+def get_recommendations_by_ids(user_movie_ids: list[int], n=5) -> list[tuple[Movie], float]:
+    """
+    this method takes in a list of movie IDs and an int n for number of results requested, takes a composite of the
+    vectors for those movies, and then compares that composite vector to the vector representation of every potential
+    match in the db\n
+    "potential matches" are defined as those movies that share at least one keyword and one genre with a user-supplied
+    movie, AND that have more than one keyword logged in the database.
+    :param user_movie_ids: a list of IDs for the movies titles submitted by the user
+    :param n: the number of results to return (default 5)
+    :return: a list of tuples (Movie object, similarity score as float)
+    """
+    user_movies = dbh.get_movies_by_ids(user_movie_ids)
+    user_vectors = [m.vector for m in user_movies]
+    genre_id_set = set() # defined as a set to avoid duplicates
+    keyword_id_set = set()
+    for m in user_movies:
+        genre_id_set.update(m.genre_ids)
+        keyword_id_set.update(m.keyword_ids)
+
+    user_composite_vector = get_composite_by_vectors(user_vectors)
+
+    potential_matches = dbh.get_potential_matches(list(genre_id_set), list(keyword_id_set))
+    [potential_matches.pop(m) for m in user_movie_ids] # make sure that we exclude movies the user entered
+    recommendation_scores = {}
+
+    for k,v in potential_matches.items():
+        recommendation_scores[k] = cosine_similarity(user_composite_vector, v)
+
+    # sort the movie IDs and scores by score and return them together as a list
+    top_movies = sorted(recommendation_scores, key=lambda x: recommendation_scores[x], reverse=True)[:n]
+    rec_scores = sorted(recommendation_scores.values(), reverse=True)[:n]
+    top_movies = dbh.get_movies_by_ids(top_movies)
+    return zip(top_movies, rec_scores)
